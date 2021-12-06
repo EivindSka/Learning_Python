4ae2c5fd1fff2037d016e662b61218ddc886b92e
Eivind Skårnes <94760983+EivindSka@users.noreply.github.com>
2021-12-06 23:39:18 +0100

    Add files via upload

--- Temperature.py
+++ Temperature.py
@@ -1,5 +1,6 @@
 
 import time
+from typing import ValuesView
 
 # name = ""
 
@@ -64,19 +65,71 @@ import time
 #         print(i)
 
 
-# Lists
-#          0          1           2          3
-food = ["Pizza", "Hamburger", "Hot Dawg", "Pasta"]
+# # Lists
+# #          0          1           2          3
+# food = ["Pizza", "Hamburger", "Hot Dawg", "Pasta"]
 
-food.append("Sushi")
-food.remove("Pasta")
-# Will remove the last index of the list
-food.pop()
-# Replaces index 1 with Tikkamasala
-food.insert(1, "Tikkamasala")
-food.sort()
+# food.append("Sushi")
+# food.remove("Pasta")
+# # Will remove the last index of the list
+# food.pop()
+# # Replaces index 1 with Tikkamasala
+# food.insert(1, "Tikkamasala")
+# food.sort()
+# # Will remove every value in the list
+# food.clear()
 
-print(food[1:3])
+# print(food[1:3])
 
-for x in food:
-    print(x)
+# for x in food:
+#     print(x)
+
+
+# utensils = {"fork", "spoon", "knife"}   # set, unordered, and unindex
+# dishes = {"bowl", "plate", "cup": 20}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.update(dishes)
+
+# dinnertable = utensils.union(dishes)
+
+# for x in dinnertable:
+#     print(x)
+
+
+#           Key         Value
+# capitals = {"USA": "Washington DC",
+#             "India": "New Dehli",
+#             "China": "Beijing",
+#             "Russia": "Moscow"}
+
+# capitals.update({"Germany": "Berlin"})
+
+# capitals.pop("China")                 Vil fjerne paret
+# print(capitals["Germany"])
+# print(capitals.get("Germany"))        Check if the key is in the dictionary
+# print(capitals.keys())                Countries
+# print(capitals.values())              Capitals
+# print(capitals.items())               Vil printe parene
+
+# for key, value in capitals.items():
+#     print(key, value)
+
+
+# def say_hi(first_name, last_name, age):
+#     # F-string gjør det lettere å legge inn parmetre
+#     print(f"Hello, {first_name} {last_name}")
+#     # print("Hello, " + first_name + " " + last_name)     # "Alternativ måte" med samme utfall
+#     print(f"You are {age} years old ")
+
+
+# say_hi("Eivind", "Skårnes", 22)
+
+
+# def multiply(number1, number2):
+#     return number1 * number2
+
+
+# x = multiply(6, 8)
+# print(x)
