bc3923d60d580b9ec56999fea324bbfc6047b36b
Eivind Skårnes <94760983+EivindSka@users.noreply.github.com>
2021-12-17 11:58:44 +0100

    Add files via upload

---
+++ lamdba.py
@@ -0,0 +1,16 @@
+
+
+# // Lambda //
+# def double(x):
+#     return x * 2
+
+
+# print(double(10))
+
+
+def double(x): return x * 2  # double = lambda x:x * 2
+def multiply(x, y): return x * y  # multiply = lambda x, y: x * y
+
+
+print(double(5))
+print(multiply(10, 9))
