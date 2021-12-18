324ab1e2a5b3d261a67a014117e5ea7326f72bfc
Eivind Skårnes <94760983+EivindSka@users.noreply.github.com>
2021-12-18 11:57:44 +0100

    Add files via upload

---
+++ dot.py
@@ -0,0 +1,15 @@
+
+
+students = ['Squidward', 'Spongebob', 'Sandy', 'Plankton', 'Mr. Krabs']
+
+students1 = ('Squidward', 'Spongebob', 'Sandy', 'Plankton', 'Mr. Krabs')
+
+# students.sort(reverse=True)  # For tuple
+
+sorted_students = sorted(students1)
+
+# for i in students:
+#    print(i)
+
+for i in sorted_students:
+    print(i)
