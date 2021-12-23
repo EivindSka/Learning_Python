8629d2aa49dc6e255401246dd037b06e95801dc8
Eivind Skårnes <94760983+EivindSka@users.noreply.github.com>
2021-12-23 14:08:49 +0100

    Add files via upload

---
+++ tkinter.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# widget = GUI elemtes: buttons, textbox, label, image
+# windows= serves as a container to hold or contain these widgets
+
+window = Tk()  # instanciate a instance of  a windows
+window.geometry('420x420')
+window.titlefunction('Window Windows')
+
+window.mainloop()
