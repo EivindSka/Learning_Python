ad4bf47c2a4ccd13c5f3eb9fb4c29b6829cf5b5b
Eivind Skårnes <94760983+EivindSka@users.noreply.github.com>
2021-12-29 13:04:17 +0100

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,20 @@
+
+
+# // List //
+
+names = ['Eivind', 'Ørjan', 'William', 'Sander']
+
+for name in names:
+    print(name)
+
+# // Key Value Pairs //
+
+person = {'name': 'Eivind',
+          'age': 22,
+          'adress': 'Norway'}
+
+for key, value in person.items():
+    print(f'key: {key} value: {value}')
+
+# for key in person:
+#     print(f'key: {key} value: {person[key]}')
